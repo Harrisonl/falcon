db70eafaa67d202ae51611cdc7a870da6d202c2e
Harry Lucas <harry@lucasconsulting.com.au>
2017-05-21 11:19:19 +1000

    first commit

---
+++ lib/falcon.ex
@@ -0,0 +1,18 @@
+defmodule Falcon do
+  @moduledoc """
+  Documentation for Falcon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Falcon.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/falcon_test.exs
@@ -0,0 +1,8 @@
+defmodule FalconTest do
+  use ExUnit.Case
+  doctest Falcon
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
